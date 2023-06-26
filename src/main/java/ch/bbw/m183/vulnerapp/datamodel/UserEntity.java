f0110fb8fd10381ad21d6d163cd57c1fc560f574
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-06-26 14:49:43 +0200

    remove unused import

--- src/main/java/ch/bbw/m183/vulnerapp/datamodel/UserEntity.java
+++ src/main/java/ch/bbw/m183/vulnerapp/datamodel/UserEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
